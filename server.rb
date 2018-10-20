1a858a6f011132de4c6ed16618e4eb5991cfc501
Ryan Horricks <ethereal@acidfactory.ca>
2018-10-19 17:39:55 -0700

    Functional redirect from shortened URL

--- server.rb
+++ server.rb
@@ -3,6 +3,7 @@ require 'base64'
 require 'pstore'
 
 get '/:url' do
+  redirect 'http://' + ShortURL.read(params[:url])
 end
 
 get '/' do
@@ -12,12 +13,31 @@ end
 post '/' do
   url = generate_short_url(params[:url])
 
-  "New URL added: #{params[:url]}"+\
-  "\nYour shortened URL is #{url}\n"
+  "New URL added: #{params[:url]}"\
+    "\nYour shortened URL is #{url}\n"
 end
 
 LETTERS = Array('a'..'z')
 def generate_short_url(original)
-  # Array.new(6) { LETTERS.sample }.join
-  "localhost:4567/" + Base64.encode64(original)[0..6]
+
+  ShortURL.save(Base64.encode64(original)[0..6], original)
+
+  # Array.new(6) { LETTERS.sample }.join #Alternative Hashing Method
+
+  'localhost:4567/' + Base64.encode64(original)[0..6]
+end
+
+# logic behind persistence of shortened URL's
+class ShortURL
+  def self.save(encoded, original)
+    store.transaction { |t| store[encoded] = original  }
+  end
+
+  def self.read(encoded)
+    store.transaction {  store[encoded] }
+  end
+
+  def self.store
+    @store ||= PStore.new('shortened_urls.db')
+  end
 end
